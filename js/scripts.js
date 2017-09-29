1032fae8748f2eadcdf19644768e0b5fed89db3b
Scott Reichert <scottreichert86@gmail.com>
2017-09-29 14:21:07 -0700

    add backend js, new order selector for html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
 // Back end logic
-function Pizza(size, topping) {
-  this.pizzaSize = size;
-  this.pizzaTopping = topping;
+function Pizza(order, size, topping) {
+  this.order = order;
+  this.size = size;
+  this.topping = topping;
   this.price = 0;
 }
-Pizza.prototype.price = function(sizeValue, toppingValue) {
+Pizza.prototype.newPrice = function(sizeValue, toppingValue) {
   this.price = sizeValue + toppingValue;
 }
-
-
+var arrayAlpha = [];
+var totalCalculator = function(pizzas) {
+  var total = 0;
+  pizzas.forEach(function(pizza1) {
+    total += pizza1.price;
+  });
+  return total;
+}
 $(document).ready(function() {
   $("form").submit(function(event) {
   event.preventDefault();
-  var pizzaOne = new Pizza($("#sizes :selected").text(), $("#toppings :selected").text());
+  var pizzaOne = new Pizza($("#orders").val(), $("#sizes :selected").text(), $("#toppings :selected").text());
   var size = parseInt($("#sizes").val());
   var topping = parseInt($("#toppings").val());
   console.log(pizzaOne);
+  console.log(size);
+  pizzaOne.newPrice(size, topping);
+  arrayAlpha.push(pizzaOne);
+  $("#output1").append("<li>" + pizzaOne.order + ":$" + pizzaOne.price + "</li>");
+  alert("here");
+
 });
 });
